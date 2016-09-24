804b6cca67e83b14bdd625af9ed766188b7c6055
John Brayton <jbrayton@goldenhillsoftware.com>
2016-09-23 20:45:57 -0400

    Framework target

---
+++ IFUnicodeURL/IFUnicodeURL/IFUnicodeURL.h
@@ -0,0 +1,19 @@
+//
+//  IFUnicodeURL.h
+//  IFUnicodeURL
+//
+//  Created by John Brayton on 9/23/16.
+//  Copyright © 2016 Jungle Candy Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IFUnicodeURL.
+FOUNDATION_EXPORT double IFUnicodeURLVersionNumber;
+
+//! Project version string for IFUnicodeURL.
+FOUNDATION_EXPORT const unsigned char IFUnicodeURLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IFUnicodeURL/PublicHeader.h>
+
+#import <NSURL+IFUnicodeURL.h>
